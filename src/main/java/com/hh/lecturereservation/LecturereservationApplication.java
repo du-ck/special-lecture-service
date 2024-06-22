5521f70765a13ccf08a3ff1d55afed71b688d383
wonuk(du_ck) <wonuk007@naver.com>
2024-06-23 05:51:24 +0900

    project init

---
+++ src/main/java/com/hh/lecturereservation/LecturereservationApplication.java
@@ -0,0 +1,13 @@
+package com.hh.lecturereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturereservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecturereservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hh/lecturereservation/LecturereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.lecturereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturereservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
