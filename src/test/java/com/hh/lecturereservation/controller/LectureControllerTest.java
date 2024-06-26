04c1423b3041e5e1016d0dc0ca81b44b469e22f6
wonuk(du_ck) <wonuk007@naver.com>
2024-06-26 19:34:31 +0900

    test 클래스 이름 변경

--- src/test/java/com/hh/lecturereservation/controller/LectureControllerTest.java
+++ src/test/java/com/hh/lecturereservation/controller/LectureControllerTest.java
@@ -32,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LectureController.class)
-class LectureEntityControllerTest {
+class LectureControllerTest {
 
     @Autowired
     MockMvc mockMvc;
--- src/test/java/com/hh/lecturereservation/domain/LectureServiceTest.java
+++ src/test/java/com/hh/lecturereservation/domain/LectureServiceTest.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class LectureEntityServiceTest {
+class LectureServiceTest {
 
     @InjectMocks
     private LectureService lectureService;
